ec5d70f5f0b26f6a384dce44eae0f92d74482aab
jun-yunan <junyunan341695@gmail.com>
2024-12-20 18:23:53 +0700

    feat: update BlogItem component to use User type in HoverCardProfile and remove unused imports

--- app/(main)/blogs/_components/blog-item.tsx
+++ app/(main)/blogs/_components/blog-item.tsx
@@ -1,20 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
-import { Author, Blog } from '@/types/blog';
+import { Card } from '@/components/ui/card';
+import { Blog } from '@/types/blog';
 import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import { HoverCardProfile } from '../../_components/hover-card-profile';
 import { format } from 'date-fns';
-import {
-  Heart,
-  MessageCircle,
-  MoreVertical,
-  Share,
-  ThumbsUp,
-} from 'lucide-react';
-import ButtonInteractBlog from '../../(user)/profile/_components/button-interact-blog';
-import { Separator } from '@/components/ui/separator';
+import { MoreVertical } from 'lucide-react';
+
 import dynamic from 'next/dynamic';
+import { User } from '@/types/user';
 
 interface BlogItemProps {
   blog: Blog;
@@ -29,7 +23,7 @@ const BlogItem: FunctionComponent<BlogItemProps> = ({ blog }) => {
     <Card className="flex flex-col items-start p-6 gap-y-4">
       <div className="w-full flex items-center justify-between">
         <div className="flex items-center gap-x-2">
-          <HoverCardProfile<Author> information={blog.author}>
+          <HoverCardProfile<User> information={blog.author}>
             <Avatar className="hover:opacity-75 transition-all duration-300 w-11 h-11 cursor-pointer">
               <AvatarImage
                 src={blog.author.imageUrl}
