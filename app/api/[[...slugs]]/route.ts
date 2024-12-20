3e7a0951ba8efbc5dd50378e272a57242cf0e635
jun-yunan <junyunan341695@gmail.com>
2024-12-20 18:28:04 +0700

    feat: remove JWT middleware from API route and update build script to include Prisma generation

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,6 +1,5 @@
 import { swagger } from '@elysiajs/swagger';
 import { cors } from '@elysiajs/cors';
-import { jwt } from '@elysiajs/jwt';
 import { Elysia } from 'elysia';
 import { auth } from '../_plugins/auth';
 import { blog } from '../_plugins/blog';
@@ -12,12 +11,6 @@ const app = new Elysia({
 })
   .use(swagger())
   .use(cors())
-  .use(
-    jwt({
-      name: 'jwt',
-      secret: process.env.SECRET_JWT!,
-    }),
-  )
   .get('/', () => 'Hello Elysia')
   .use(auth)
   .use(blog)
