8d5b7e7e066245f9821f5c30df4205b025fd569d
jun-yunan <junyunan341695@gmail.com>
2024-12-20 18:09:57 +0700

    feat: migrate comment and auth controllers to plugins for improved structure

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -2,10 +2,10 @@ import { swagger } from '@elysiajs/swagger';
 import { cors } from '@elysiajs/cors';
 import { jwt } from '@elysiajs/jwt';
 import { Elysia } from 'elysia';
-import { auth } from '../_controllers/auth';
-import { blog } from '../_controllers/blog';
-import { user } from '../_controllers/user';
-import { comment } from '../_controllers/comment';
+import { auth } from '../_plugins/auth';
+import { blog } from '../_plugins/blog';
+import { user } from '../_plugins/user';
+import { comment } from '../_plugins/comment';
 
 const app = new Elysia({
   prefix: '/api',
